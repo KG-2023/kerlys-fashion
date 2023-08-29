457767b154071471c5b29bed77ddbaa75df2a769
Edgar Pateño <edgarpateno80@gmail.com>
2023-08-29 16:40:10 +0800

    made slight changes (non-bearing) on the index and footer.js files

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@
 fetch('/footer.html')
     .then(response => response.text())
     .then(data => {
-        // Insert the contents into the main-nav-placeholder element
+        // Insert the contents into the footer element
         document.getElementById('footer').innerHTML = data;
     })
     .catch(error => {
